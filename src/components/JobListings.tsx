f9413891c9bbf7f94c87c427f2e2a051e48cad78
Sweta7 <anvita1982@gmail.com>
2024-07-05 22:51:45 +1200

    fetch data from bff

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import jobs from '../jobs.json'
 import JobListing from './JobListing';
+import { useState, useEffect } from 'react';
+import Spinner from './Spinner';
 
 export interface JobType {
   id: string;
@@ -23,19 +24,40 @@ interface JobListingsProps {
 
 
 const JobListings : React.FC<JobListingsProps> = ({isHome = false}) => {
-  const jobLists = isHome? jobs.slice(0,3): jobs;
+  const [jobs, setJobs] = useState([])
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const fetchJobs = async () =>{
+      try {
+        const apiUrl = isHome ? 'api/jobs?_limit=3' : 'api/jobs'
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchJobs()
+  })
+
   return (
     <section className="bg-blue-50 px-4 py-10">
     <div className="container-xl lg:container m-auto">
       <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
        {isHome ?  'Recent Jobs': 'All Jobs'}
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-      {jobLists.map((job: JobType) => (
-       <JobListing key={job.id} job={job}/>
-        ))}
-     
-      </div>
+      {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {jobs.map((job:JobType) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+          )}
     </div>
   </section>
   )
